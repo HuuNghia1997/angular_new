71db41c3bd710ec6d7a4f2719cc9677b5f11af26
Trankadzo <trankadzo12345@gmail.com>
2020-12-30 15:17:24 +0700

    first commit

---
+++ src/app/modules/category_management/pages/list-category/list-category-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {DefaultLayoutComponent} from 'layout/default/default-layout/default-layout.component';
+import { ListCategoryComponent } from './list-category/list-category.component';
+
+const routes: Routes = [
+  { path: '',
+    component: DefaultLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: ListCategoryComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ListCategoryRoutingModule { }
